d22a4709f9d34448f79aa71b38293235c82adbca
luo0818 <57941664+luo0818@users.noreply.github.com>
2020-03-29 15:09:04 +0800

    first

---
+++ angulardemo/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NewsComponent } from './components/news/news.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewsComponent,
+    HomeComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
